4e91c61431aef12eabc0c71ee75f5be31bcd2235
Bao Trong <nhbaotrong@gmail.com>
2025-09-30 14:01:53 +0700

    chore: update debouced for search on user navbar

--- src/components/layout/sidebar/user-sidebar.tsx
+++ src/components/layout/sidebar/user-sidebar.tsx
@@ -1,7 +1,8 @@
 'use client'
 
 import { Divider, TextInput } from '@mantine/core'
-import { useState } from 'react'
+import { useDebouncedValue } from '@mantine/hooks'
+import { useEffect, useState } from 'react'
 import { CiSearch } from 'react-icons/ci'
 import { FaHome, FaMoneyCheckAlt, FaPhotoVideo, FaTools, FaUser } from 'react-icons/fa'
 import { FaMessage } from 'react-icons/fa6'
@@ -9,50 +10,63 @@ import NavbarLink from './navbar-link'
 
 export default function UserSidebar() {
      const [active, setActive] = useState(0)
+     const [searchValue, setSearchValue] = useState('')
+     const [debounced] = useDebouncedValue(searchValue, 750)
 
-     const sidebarItems = [
-          {
-               label: 'Forum',
-               icon: <FaHome />,
-               href: '/forum'
-          },
-          {
-               label: 'Profile',
-               icon: <FaUser />,
-               href: '/profile/36'
-          },
-          {
-               label: 'Message',
-               icon: <FaMessage />,
-               href: '/message'
-          },
-          {
-               label: 'Purchases',
-               icon: <FaMoneyCheckAlt />,
-               href: '/purchases'
-          },
-          {
-               label: 'Gallery',
-               icon: <FaPhotoVideo />,
-               href: '/gallery'
-          },
-     ]
+     useEffect(() => {
+          console.log(`SEARCH SIDEBAR: ${debounced}`)
+     }, [debounced])
 
-     const sideBarItemsSetting = [
+     const sidebar = [
           {
-               label: 'Setting',
-               icon: <FaTools />,
-               href: '/setting'
+               title: 'MENU',
+               subs: [
+                    {
+                         label: 'Forum',
+                         icon: <FaHome />,
+                         href: '/forum'
+                    },
+                    {
+                         label: 'Profile',
+                         icon: <FaUser />,
+                         href: '/profile/36'
+                    },
+                    {
+                         label: 'Message',
+                         icon: <FaMessage />,
+                         href: '/message'
+                    },
+                    {
+                         label: 'Purchases',
+                         icon: <FaMoneyCheckAlt />,
+                         href: '/purchases'
+                    },
+                    {
+                         label: 'Gallery',
+                         icon: <FaPhotoVideo />,
+                         href: '/gallery'
+                    },
+               ]
           },
           {
-               label: 'Help',
-               icon: <FaTools />,
-               href: '/setting'
-          },
-          {
-               label: 'Supscriptions',
-               icon: <FaTools />,
-               href: '/setting'
+               title: 'Settings',
+               subs: [
+                    {
+                         label: 'Setting',
+                         icon: <FaTools />,
+                         href: '/setting'
+                    },
+                    {
+                         label: 'Help',
+                         icon: <FaTools />,
+                         href: '/setting'
+                    },
+                    {
+                         label: 'Supscriptions',
+                         icon: <FaTools />,
+                         href: '/setting'
+                    }
+               ]
           }
      ]
 
@@ -66,28 +80,23 @@ export default function UserSidebar() {
                          placeholder='Search'
                          leftSection={<CiSearch />}
                          className='my-2'
+                         onChange={(e) => setSearchValue(e.currentTarget.value)}
                     />
-                    <h1 className='text-neutral-600 text-sm py-1'>MAIN MENU</h1>
-                    {sidebarItems.map((item, index) =>
-                         <NavbarLink
-                              key={index}
-                              href={item.href}
-                              label={item.label}
-                              icon={item.icon}
-                              isActive={index === active}
-                              onClick={() => setActive(index)}
-                         />
-                    )}
-                    <h1 className='text-neutral-600 text-sm py-1'>Setting</h1>
-                    {sideBarItemsSetting.map((item, index) =>
-                         <NavbarLink
-                              key={index}
-                              href={item.href}
-                              label={item.label}
-                              icon={item.icon}
-                              isActive={index === active}
-                              onClick={() => setActive(index)}
-                         />
+
+                    {sidebar.map((item, index) =>
+                         <div key={index}>
+                              <h1 className='text-neutral-600 text-sm py-1'>{item.title}</h1>
+                              {item.subs.map((sub, index) =>
+                                   <NavbarLink
+                                        key={index}
+                                        href={sub.href}
+                                        label={sub.label}
+                                        icon={sub.icon}
+                                        isActive={index === active}
+                                        onClick={() => setActive(index)}
+                                   />
+                              )}
+                         </div>
                     )}
                </div>
           </nav>
