79ada9eaa66197dc07d318572235f7bd51780a52
Aries <lehuy25032004@gmail.com>
2025-10-23 23:31:54 +0700

    feat(marketplace): add pagination

--- src/app/(public-layout)/marketplace/components/marketplace-grid.tsx
+++ src/app/(public-layout)/marketplace/components/marketplace-grid.tsx
@@ -1,5 +1,8 @@
-import React from 'react';
+'use client'
+
+import React, { useState } from 'react';
 import MarketplaceItemCard from './marketplace-item-card';
+import { Pagination, PaginationContent, PaginationEllipsis, PaginationItem, PaginationLink, PaginationNext, PaginationPrevious } from '@/components/ui/pagination';
 
 interface MarketplaceItem {
     id: number;
@@ -20,6 +23,22 @@ interface MarketplaceGridProps {
 }
 
 export default function MarketplaceGrid({ items, searchQuery }: MarketplaceGridProps) {
+    const [currentPage, setCurrentPage] = useState(1)
+    const itemsPerPage = 9
+    const totalPage = Math.ceil(items.length / itemsPerPage)
+
+    const handlePrevious = () => {
+        if (currentPage > 1) setCurrentPage(currentPage - 1)
+    }
+
+    const handleNext = () => {
+        if (currentPage < totalPage) setCurrentPage(currentPage + 1)
+    }
+
+    const handlePageChange = (page: number) => {
+        setCurrentPage(page)
+    }
+
     if (searchQuery && items.length === 0) {
         return (
             <div className='text-center py-10 text-muted-foreground'>
@@ -29,11 +48,78 @@ export default function MarketplaceGrid({ items, searchQuery }: MarketplaceGridP
         );
     }
 
+    //Cai nay de filter item moi trang
+    const startIndex = (currentPage - 1) * itemsPerPage
+    const endIndex = startIndex + itemsPerPage
+    const currentItems = items.slice(startIndex, endIndex)
+
     return (
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-            {items.map(item => (
-                <MarketplaceItemCard key={item.id} item={item} />
-            ))}
+        <div className='flex flex-col gap-8'>
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+                {currentItems.map(item => (
+                    <MarketplaceItemCard key={item.id} item={item} />
+                ))}
+            </div>
+
+            <Pagination>
+                <PaginationContent>
+                    <PaginationItem>
+                        <PaginationPrevious href="#" onClick={handlePrevious} />
+                    </PaginationItem>
+
+                    <PaginationItem>
+                        <PaginationLink
+                            href="#"
+                            isActive={currentPage === 1}
+                            onClick={() => handlePageChange(1)}
+                        >
+                            1
+                        </PaginationLink>
+                    </PaginationItem>
+
+                    {currentPage > 3 && <PaginationEllipsis />}
+                    
+                    {Array.from({ length: totalPage }, (_, i) => i + 1)
+                        .filter(
+                            (page) =>
+                                page !== 1 &&
+                                page !== totalPage &&
+                                page >= currentPage - 1 &&
+                                page <= currentPage + 1
+                        )
+                        .map((page) => (
+                            <PaginationItem key={page}>
+                                <PaginationLink
+                                    href="#"
+                                    isActive={page === currentPage}
+                                    onClick={() => handlePageChange(page)}
+                                >
+                                    {page}
+                                </PaginationLink>
+                            </PaginationItem>
+                        ))}
+
+                    
+                    {currentPage < totalPage - 2 && <PaginationEllipsis />}
+                    
+                    {totalPage > 1 && (
+                        <PaginationItem>
+                            <PaginationLink
+                                href="#"
+                                isActive={currentPage === totalPage}
+                                onClick={() => handlePageChange(totalPage)}
+                            >
+                                {totalPage}
+                            </PaginationLink>
+                        </PaginationItem>
+                    )}
+
+                    <PaginationItem>
+                        <PaginationNext href="#" onClick={handleNext} />
+                    </PaginationItem>
+                </PaginationContent>
+            </Pagination>
+
         </div>
     );
 }
